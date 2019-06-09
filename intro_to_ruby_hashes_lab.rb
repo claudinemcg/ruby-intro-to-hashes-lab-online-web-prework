fc5fe515fc314369559c050eed39b9feef403420
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 19:58:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {}
-	     }
+	     },
 	    :rent_in_dollars => 
 	      {
 	        :one_piece_owned => 25, 
@@ -51,5 +51,4 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {
 	}
-	  
 end
